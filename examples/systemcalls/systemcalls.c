73d1df24c747c4055d73569809c6320b8abe4d76
Ahmad Hatami <hhatamdev@outlook.com>
2024-01-26 09:18:20 -0800

    Assignment-3 Part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,9 @@
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -9,15 +14,28 @@
 */
 bool do_system(const char *cmd)
 {
+  bool b_result = false;
+  /*
+   * TODO  add your code here
+   * Call the system() function with the command set in the cmd
+   *   and return a boolean true if the system() call completed with success
+   *   or false() if it returned a failure
+   */
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
+  int ret = system(cmd);
 
-    return true;
+  if(ret == -1)
+  {
+    perror("do_system, ");   
+  } 
+  else
+  {
+    if(WEXITSTATUS(ret) == EXIT_SUCCESS)
+    {
+      b_result = true;
+    }
+  }
+  return b_result;
 }
 
 /**
@@ -39,16 +57,38 @@ bool do_exec(int count, ...)
     va_list args;
     va_start(args, count);
     char * command[count+1];
+    char *p_path = NULL;
     int i;
+    pid_t pid;
+    bool b_result = false;
+    char *p_char = NULL;
+    
     for(i=0; i<count; i++)
     {
-        command[i] = va_arg(args, char *);
+        if(i==0)
+        {
+          p_path = va_arg(args, char *);
+          
+          //printf("p_path:%s\n", p_path);
+          p_char = strrchr(p_path,'/');
+          if(NULL != p_char)
+          {
+            command[i] = (p_char + 1);
+          }
+          else
+          {
+            //printf("No explicit path\n");
+            command[i] = p_path;
+          }
+        }
+        else
+        {
+          command[i] = va_arg(args, char *);
+        }
+        //printf("command[%d]=%s\n",i, command[i]);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-
+    
 /*
  * TODO:
  *   Execute a system command by calling fork, execv(),
@@ -60,30 +100,113 @@ bool do_exec(int count, ...)
 */
 
     va_end(args);
+       
+    pid = fork();
+    
+    if(pid == -1)
+    {
+      perror("do_exec, fork()");
+    }
+    else if(pid == 0)
+    {
+      /* The child process */
+      int status = execv(p_path,command);
 
-    return true;
+      if(status == -1)
+      {
+         perror("do_exec, execv(), "); 
+      }
+      else
+      {
+        b_result = true;
+      }
+    }
+    else
+    {
+      int child_status;
+      /* Parent process */
+      pid_t this_pid = waitpid(pid,&child_status,0);
+
+      if(this_pid == -1)
+      {
+        perror("do_exec, wait(), ");
+      }
+      else if( WIFEXITED(child_status) )
+      {
+        /* command terminated normally, check exit status */
+        if( WEXITSTATUS(child_status) == EXIT_SUCCESS )
+        {
+          b_result = true;
+        }
+      }
+    }
+    
+    return b_result;
 }
 
 /**
 * @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
+*   This file will b-e closed at completion of the function call.
 * All other parameters, see do_exec above
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char * command[count+2];
     int i;
-    for(i=0; i<count; i++)
-    {
-        command[i] = va_arg(args, char *);
-    }
-    command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
+    char *p_path = NULL;
+    char *p_char = NULL;
+    bool b_result = false;
+    char *p_redirect_msg = NULL;
+    char redirect_op[] = " > ";
+    pid_t pid;
+    
+    putenv("HOME=\"\"");
+    
 
+    for (i = 0; i < count; i++)
+    {
+      if(i == 0)
+      {
+          p_path = va_arg(args, char *);
+
+          //printf("do_exec_redirect(), p_path:%s\n", p_path);
+          p_char = strrchr(p_path, '/');
+          if (NULL != p_char)
+          {
+            command[i] = (p_char + 1);
+          }
+          else
+          {
+            //  printf("do_exec_redirect(), No explicit path\n");
+            command[i] = p_path;
+          }
+      }
+      else
+      {
+        command[i] = va_arg(args, char *);
+      }
+      //printf("command[%d]=%s\n", i, command[i]);
+    }
+    va_end(args);
+
+    p_redirect_msg = malloc(strlen(outputfile) + strlen(redirect_op));
+    if (NULL != p_redirect_msg)
+    {
+      strcpy(p_redirect_msg, redirect_op);
+      strcat(p_redirect_msg, outputfile);
+      command[count] = p_redirect_msg;
+      //printf("command[%d]=%s\n", count, command[i]);
+    }
+    else
+    {
+      printf("Memory allocation error\n");
+      return false;
+    }
+    
+    command[count+1] = NULL;
+    
 
 /*
  * TODO
@@ -93,7 +216,47 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *
 */
 
-    va_end(args);
+    pid = fork();
+    
+    if (pid == -1)
+    {
+      perror("do_exec, fork()");
+    }
+    else if (pid == 0)
+    {
+      /* The child process */
+      int status = execv(p_path, command);
 
-    return true;
+      if (status == -1)
+      {
+        perror("do_exec_redirect, execvp(), ");
+      }
+      else
+      {
+        b_result = true;
+      }
+    }
+    else
+    {
+        int child_status;
+        /* Parent process */
+        
+        pid_t this_pid = waitpid(pid, &child_status, 0);
+
+        if (this_pid == -1)
+        {
+            perror("do_exec, wait(), ");
+        }
+        else if (WIFEXITED(child_status))
+        {
+            /* command terminated normally, check exit status */
+            if (WEXITSTATUS(child_status) == EXIT_SUCCESS)
+            {
+                b_result = true;
+            }
+        }
+        free(p_redirect_msg);
+    }
+
+    return b_result;
 }
